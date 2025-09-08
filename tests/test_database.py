977af6bc0437830053bab1089377f0a907ef1ef1
Yovana-Palencia <174197@upslp.edu.mx>
2025-09-08 02:03:01 -0600

    Update test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -18,10 +18,20 @@ def test_alumnos_insertados(db_connection):
         cur.execute("SELECT COUNT(*) FROM alumnos;")
         count = cur.fetchone()[0]
         assert count == 10
+def test_estudiante_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM estudiante;")
+        count = cur.fetchone()[0]
+        assert count == 10
 
 def test_maestros_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM maestros;")
+        count = cur.fetchone()[0]
+        assert count == 10
+def test_profesor_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM profesor;")
         count = cur.fetchone()[0]
         assert count == 10
 
@@ -30,13 +40,28 @@ def test_grupos_insertados(db_connection):
         cur.execute("SELECT COUNT(*) FROM grupos;")
         count = cur.fetchone()[0]
         assert count == 10
+def test_curso_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM curso;")
+        count = cur.fetchone()[0]
+        assert count == 10
 
 def test_inscripciones_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM inscripciones;")
         count = cur.fetchone()[0]
         assert count == 10  
+def test_inscripcion_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM inscripcion;")
+        count = cur.fetchone()[0]
+        assert count == 10  
 
+def test_asistencia_insertados(db_connection):
+    with db_connection.cursor() as cur:
+        cur.execute("SELECT COUNT(*) FROM asistencia;")
+        count = cur.fetchone()[0]
+        assert count == 10 
 def test_asistencia_insertados(db_connection):
     with db_connection.cursor() as cur:
         cur.execute("SELECT COUNT(*) FROM asistencia;")
@@ -49,9 +74,14 @@ def test_structure(db_connection):
             WHERE tablename 
             IN ('alumnos', 'maestros', 'grupos',
             'inscripciones', 'asistencia');
+            IN ('estudiante', 'profesor', 'curso',
+            'inscripcion', 'asistencia');
           '''
     expected_tables = {'alumnos', 'maestros',
                        'grupos', 'inscripciones',
+                       'asistencia'}
+    expected_tables = {'estudiante', 'profesor',
+                       'curso', 'inscripcion',
                        'asistencia'}
     with db_connection.cursor() as cur:
         cur.execute(sql)
