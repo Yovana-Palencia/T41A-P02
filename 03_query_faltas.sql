79db297410a7faa166274e84735d97339f6d9514
Yovana-Palencia <174197@upslp.edu.mx>
2025-09-08 01:46:47 -0600

    Update 03_query_faltas.sql

--- 03_query_faltas.sql
+++ 03_query_faltas.sql
@@ -4,3 +4,13 @@
     JOIN grupos g ON a.periodo = g.periodo AND a.seccion = g.seccion
     GROUP BY g.periodo, g.seccion, g.nombre_grupo
     ORDER BY g.seccion;
+SELECT e.matricula,
+       e.nombre AS nombre_estudiante,
+       c.nombre AS nombre_curso,
+       c.grupo,
+       COUNT(*) FILTER (WHERE a.asistio = FALSE) AS total_faltas
+FROM asistencia a
+JOIN estudiante e ON a.matricula = e.matricula
+JOIN curso c ON a.nombre = c.nombre AND a.grupo = c.grupo
+GROUP BY e.matricula, e.nombre, c.nombre, c.grupo
+ORDER BY e.matricula;
